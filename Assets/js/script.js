689817ae62d6d54ad34e9b97314e03d0dd706e3a
Jeff <obbynetwork@gmail.com>
2022-04-13 01:37:07 -0700

    Simple css mvp

---
+++ Assets/js/script.js
@@ -0,0 +1,9 @@
+startButton = document.getElementById("startButton");
+highscores = document.getElementById("highscores");
+
+startButton.addEventListener("click", function () {
+    console.log('You have started the quiz');
+})
+highscores.addEventListener("click", function () {
+    console.log('Your Highscores :');
+})

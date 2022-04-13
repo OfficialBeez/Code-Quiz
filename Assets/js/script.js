3cfff1f8ad4d9b8cf1235aea4ec0ff870451005d
Jeff <obbynetwork@gmail.com>
2022-04-13 02:36:34 -0700

    Added Timer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,16 @@
 startButton = document.getElementById("startButton");
 highscores = document.getElementById("highscores");
 
+function startQuiz() {
+    timeLeft = 60;
+    var interval = setInterval(function () {
+        timeLeft--;
+        timer.textContent = 'Time : ' + timeLeft;
+    }, 1000)
+}
+timer.textContent = "Time: " + 0;
 startButton.addEventListener("click", function () {
+    startQuiz();
     console.log('You have started the quiz');
 })
 highscores.addEventListener("click", function () {

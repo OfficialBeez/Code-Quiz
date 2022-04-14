ad09b11fe2e421edb516aa5854c285bdc259dd13
Jeff <obbynetwork@gmail.com>
2022-04-13 20:48:41 -0700

    Added Display for questions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,10 @@
-startButton = document.getElementById("startButton");
-highscores = document.getElementById("highscores");
+var startButton = document.getElementById("startButton");
+var highscores = document.getElementById("highscores");
+
+var startPage = document.getElementById("startPage");
+var questionPage = document.getElementById("questionPage")
+
+timer.textContent = "Time: " + 0;
 
 function startQuiz() {
     timeLeft = 60;
@@ -7,8 +12,8 @@ function startQuiz() {
         timeLeft--;
         timer.textContent = 'Time : ' + timeLeft;
     }, 1000)
+    show(1);
 }
-timer.textContent = "Time: " + 0;
 startButton.addEventListener("click", function () {
     startQuiz();
     console.log('You have started the quiz');
@@ -16,3 +21,21 @@ startButton.addEventListener("click", function () {
 highscores.addEventListener("click", function () {
     console.log('Your Highscores :');
 })
+
+function show(index) {
+    startPage.style.display = "none";
+    questionPage.style.display = "none";
+    //scorePage.style.display = "none";
+    switch (index) {
+        case 0 :
+            startPage.style.display = "flex";
+            break;
+        case 1:
+            questionPage.style.display = "block";
+            break;
+            //case 2:
+            //scorePage.style.display = "block";
+            //break;
+        default:
+    }
+}
